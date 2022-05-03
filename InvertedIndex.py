b82fe34e38e8082e09bfa39092b22945ab790b56
Alexander Le <alexanvl@uci.edu>
2022-05-03 15:26:58 -0700

    updates
    Co-authored-by: Cedric Wing Kwong <ced-kwong29@users.noreply.github.com>
    Co-authored-by: Rayan Tighiouart <tig-github@users.noreply.github.com>
    Co-authored-by: William Mejia <wamfoo@users.noreply.github.com>

--- InvertedIndex.py
+++ InvertedIndex.py
@@ -1,15 +1,33 @@
 from collections import defaultdict
 from email.policy import default
-
+#import nltk
 '''
-
 Inverted Index = map with token as key and list of corresponding postings as values
 Postings = [document name/id token was found in, tf-idf score (for MS1, add only the term frequency)]
-
-
 '''
 
 class InvertedIndex:
-    def __init__(self):
+    def __init__(self, directory):
         self.map = defaultdict(list)
-        # setup index
+        self.dir = directory
+        
+    def indexFiles(self):
+        print("INDEXING")
+        # open directory
+        # loop through all the subfolders in directory 
+        # when no subfolders process folder
+        #   processFolder()
+    
+    # def processFolder(self, folderPath):
+        # loop through files in folder
+        #   processFile()
+    
+    # def processFile(self, filePath):
+        # open file
+        # extract content from JSON
+        # tokens = tokenizeContent()
+        # add tokens to map
+        
+    # def tokenizeContent(self, content):
+        # tokenize content
+        # return [tokens]
--- launch.py
+++ launch.py
@@ -1,17 +1,21 @@
+from msilib.schema import Directory
 from InvertedIndex import InvertedIndex
 
-
-
 ''' 
-
-
+Steps:
+1. index every file
+2. write index to a file system
+3. search query in file system
+4. show user results               
 '''
 
+
 def main():
-    invertedIndex = InvertedIndex()
-    
-    query = input("Search for something: ")
-    
+    # directory = "developer" #path
+    directory = "directorypath"
+    invertedIndex = InvertedIndex(directory)
+    invertedIndex.indexFiles()
+
     
 
 if __name__ == "__main__":
