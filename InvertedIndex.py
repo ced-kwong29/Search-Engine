0d188b944af051d39df271a01905cb5ac9eab1b4
Alexander Le <alexanvl@uci.edu>
2022-05-04 18:32:23 -0700

    counting word frequencies works
    Co-authored-by: Cedric Wing Kwong <ced-kwong29@users.noreply.github.com>
    Co-authored-by: William Mejia <wamfoo@users.noreply.github.com>
    Co-authored-by: Rayan Tighiouart <tig-github@users.noreply.github.com>

--- InvertedIndex.py
+++ InvertedIndex.py
@@ -5,23 +5,40 @@ import nltk.stem
 import os
 import json
 from bs4 import BeautifulSoup, SoupStrainer
+import psutil
 '''
 Inverted Index = map with token as key and list of corresponding postings as values
 Postings = [document name/id token was found in, tf-idf score (for MS1, add only the term frequency)]
 '''
 
-# 50 % ram
-# when we hit the threshold
-# begin offloading from disk
-# ram usage back to 0
-# repeat
 
 class InvertedIndex:
     def __init__(self, directory):
         self.map = defaultdict(lambda: defaultdict(int))
         self.dir = directory
         self.docID = 0
+        self.merge_counter = 1
+        self.memory_limit = .8
+        self.counter = 0
+        self.mergeList = {*{}}      # this is the ONLY way to write a set
+
+
+    def mergeFiles(self):
+        opened_files = [json.loads(json.load(f)) for f in self.mergeList]
+        print(opened_files)
         
+
+    def offload(self):
+        with open(f"index{self.merge_counter}", "w") as file:
+            json.dump(self.map, file)
+        self.map = {}
+        self.merge_counter += 1
+
+
+    def checkMemoryUsage(self):
+        return psutil.virtual_memory()[2]
+        
+
     # Supplementary querying
     # Milestone #
     def andQuery(self, fL, sL):
@@ -41,30 +58,41 @@ class InvertedIndex:
     def incrementID(self):
         self.docID += 1
 
-    #
+
     def indexFiles(self):
         print("INDEXING")
         for folderName in os.listdir(self.dir):
             folder = os.path.join(self.dir, folderName)
             self.processFolder(folder)
-            break
+            if self.counter >= 10:
+                break
     
+
     def processFolder(self, folderPath):
         print("FOLDER PATH: " + folderPath)
         for fileName in os.listdir(folderPath):
             filePath = os.path.join(folderPath, fileName)
             self.processFile(filePath)
-            break
+            # break
     
+
     def processFile(self, filePath):
-        print("FILE PATH: " + filePath)
-        with open(filePath, "r") as file:
-            loadedFile = json.load(file)
-            # url = loadedFile["url"]
-            content = loadedFile["content"]
-            # encoding = loadedFile["encoding"]
-            tokenList = self.tokenizeContent(content)
-            # self.computeWordDoc(tokenList)
+        if self.counter < 10:
+            print("FILE PATH: " + filePath)
+            with open(filePath, "r") as file:
+                loadedFile = json.load(file)
+                content = loadedFile["content"]
+                memory = self.checkMemoryUsage()
+                print("MEMORY: " + str(memory))
+                # if memory > self.memory_limit:
+                #     self.offload()
+                tokenList = self.tokenizeContent(content)
+                temp = self.computeWordDoc(tokenList)
+                self.updateMap(temp)
+                self.counter += 1
+        else:
+            print(f"{self.counter} files have been read.\n\n")
+
             
     def tokenizeContent(self, content):
         stemmer = nltk.stem.snowball.EnglishStemmer()
@@ -91,13 +119,27 @@ class InvertedIndex:
         print(tokens)
         return tokens
 
+    def printMap(self, map):
+        for k,v in sorted(map.items()):
+            print(k,v)
+
     def computeWordDoc(self, tokenizedList):
-        temp = {values:self.docID for values in tokenizedList}
+        temp = defaultdict(lambda: defaultdict(int))
+        for key in tokenizedList:
+            temp[key][self.docID] += 1
         self.incrementID()
+        print(temp)
+        self.printMap(temp)
         return temp
 
     def updateMap(self, tempMap):
-        for k in tempMap.keys():
-            for nk in tempMap[k]:
-                self.map[k][nk] += tempMap[k].get(nk, 0)
-  
+        for word in tempMap.keys():
+            if word not in self.map:
+                #print(word + " NOT IN MAP")
+                self.map[word] = tempMap[word]
+            else:
+                for doc in tempMap[word]:
+                    self.map[word][doc] = tempMap[word][doc]
+        self.printMap(self.map)
+        
+        
